4bfa5d199c71a98ea18ac16ff14dac85ac1ce1e9
Sakshimadke21 <sakshi.madke.sod23@anu.edu.in>
2024-07-20 15:39:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
 function setup() {
   createCanvas(700, 480);
 
-   // R,G,B values.
-   background(52,235,352);
   
-  //take a big brush
+  //R,G,B values
+  background(225,204,0);
+  
+  //takea big brush
   strokeWeight(10);
-  //dip it in color purple
-  stroke("navy");
-  frameRate(10);
   
+
 
 }
 
+
 function draw() {
-  //declare variables
-  let posX=mouseX
+  
   let posY=mouseY
-   //place a dot on a screen
-  square(30,20,55)
+  let posX=mouseX
+  point(posX,posY)
+  fill(posX,20,30,20,28)
+  stroke(10,12,50,20,60)
+  triangle(mouseX,mouseY,55,80,20)
+fill(mouseX,mouseY,30,80,20)
 }
