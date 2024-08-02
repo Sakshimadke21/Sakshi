dca2f0d1653e0b681849bfae6d782eefd8357446
Sakshimadke21 <sakshi.madke.sod23@anu.edu.in>
2024-08-02 15:17:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,31 +1,22 @@
-// Define ball properties
-let x, y;           // Position of the ball
-let xSpeed, ySpeed; // Speed of the ball
-
 function setup() {
-  createCanvas(600, 400);  // Set canvas size
-  x = width / 2;          // Start ball in the middle of the canvas
-  y = height / 2;
-  xSpeed = 5;             // Initial speed of the ball in x direction
-  ySpeed = 3;             // Initial speed of the ball in y direction
+  createCanvas(700, 480);
+
+   // R,G,B values.
+   background(52,235,352);
+  
+  //take a big brush
+  strokeWeight(10);
+  //dip it in color purple
+  stroke("navy");
+  frameRate(10);
+  
+
 }
 
 function draw() {
-  background("black");        // Set background color
-
-  // Draw the ball
-  fill("yellow");              // Ball color
-  ellipse(x, y, 50, 50);  // Draw ball with diameter 50
-
-  // Update ball position
-  x += xSpeed;
-  y += ySpeed;
-
-  // Check for collision with walls
-  if (x > width || x < 0) {
-    xSpeed *= -1;         // Reverse x direction
-  }
-  if (y > height || y < 0) {
-    ySpeed *= -1;         // Reverse y direction
-  }
+  //declare variables
+  let posX=mouseX
+  let posY=mouseY
+   //place a dot on a screen
+  square(30,20,55)
 }
